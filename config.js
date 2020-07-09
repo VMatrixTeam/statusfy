a323f8fc43e1c1828ef998eb743685fa29e974b4
Robin Wong <chaowang0313@gmail.com>
2020-07-09 10:04:02 +0800

    feat: init statusfy

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "Matrix",
+  "name": "matrix",
+  "description": "",
+  "defaultLocale": "zh",
+  "locales": [
+    {
+      "code": "zh",
+      "iso": "zh-CN",
+      "name": "中文",
+      "file": "zh.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
