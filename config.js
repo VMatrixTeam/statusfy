34714e8db3b9bc951d4ed29d4c3d53dd2314fade
Robin Wong <chaowang0313@gmail.com>
2020-07-09 10:11:12 +0800

    fix: i18n

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ module.exports = {
   "title": "Matrix",
   "name": "matrix",
   "description": "",
-  "defaultLocale": "zh-CN",
+  "defaultLocale": "zh-cn",
   "locales": [
     {
-      "code": "zh-CN",
+      "code": "zh-cn",
       "iso": "zh-CN",
       "name": "中文",
       "file": "zh.json"
